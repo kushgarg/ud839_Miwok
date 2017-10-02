b07f3960a2c143e5d133b6898135e7956a18da0a
Kush Garg <gargkush97@gmail.com>
2017-10-02 21:17:10 +0530

    added arrayAdapter and arrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +30,25 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<word> words = new ArrayList<word>();
+
+        words.add(new word("one","lutti"));
+        words.add(new word("two","otiiko"));
+        words.add(new word("three","tolookosu"));
+        words.add(new word("four","oyyisa"));
+        words.add(new word("five","massokka"));
+        words.add(new word("six","temmokka"));
+        words.add(new word("seven","kenekaku"));
+        words.add(new word("eight","kawinta"));
+        words.add(new word("nine","wo'e"));
+        words.add(new word("ten","na'aacha"));
+
+        ArrayAdapter<word> itemsAdapter = new ArrayAdapter<word>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/word.java
@@ -0,0 +1,18 @@
+package com.example.android.miwok;
+
+/**
+ * Created by MK on 02-10-2017.
+ */
+
+public class word {
+
+    private String original;
+
+    private String translation;
+
+    public word (String or, String trans)
+    {
+        original = or;
+        translation = trans;
+    }
+}
